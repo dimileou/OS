331c0745677d1b4d3574bc70efddf4fe877fce7a
dimileou <leoudimilena@gmail.com>
2024-12-05 15:32:56 +0200

    First Lab

--- OS/Lab2/functions.h
+++
@@ -1,9 +0,0 @@
-// functions.h
-#ifndef FUNCTIONS_H
-#define FUNCTIONS_H
-
-// Function declarations
-void greet();
-int add(int a, int b);
-
-#endif
--- OS/Lab2/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-date
-current_time=$(date +%s)
-echo "$current_time"
-let current_time=current_time*2
-echo "$current_time"
-for i in {1..20}; do
-echo "Number: $i"
-done
